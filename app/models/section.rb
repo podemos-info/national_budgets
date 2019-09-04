b22bf9d811f9ac54d358cbfa9fc6c8d737f3373d
Jose <joseluispodemos@gmail.com>
2019-09-04 22:22:03 +0200

    Fix references

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,4 @@
 class Section < ApplicationRecord
   belongs_to :budget
+  has_many :service
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,3 @@
 class Service < ApplicationRecord
-  belongs_to :budget, through: :section
   belongs_to :section
 end
