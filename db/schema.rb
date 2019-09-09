89e8ba84dde30dc03a1cb8b18502f4c39ce5cbb7
Jose <joseluispodemos@gmail.com>
2019-09-09 09:24:04 +0200

    Add status to amendments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_065816) do
+ActiveRecord::Schema.define(version: 2019_09_09_071150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_09_06_065816) do
     t.bigint "budget_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", null: false
     t.index ["budget_id"], name: "index_amendments_on_budget_id"
     t.index ["user_id"], name: "index_amendments_on_user_id"
   end
