125f8ef4064d82f8fcf6eb403fb4bf0961ef6a19
Jose <joseluispodemos@gmail.com>
2019-09-09 13:54:31 +0200

    Change inheritance of amendments controller

--- app/controllers/amendments_controller.rb
+++ app/controllers/amendments_controller.rb
@@ -1,9 +1,72 @@
-class AmendmentsController < InheritedResources::Base
+class AmendmentsController < ApplicationController
+  before_action :set_amendment, only: [:show, :edit, :update, :destroy]
+
+  # GET /amendments
+  # GET /amendments.json
+  def index
+    @amendments = Amendment.all
+  end
+
+  # GET /amendments/1
+  # GET /amendments/1.json
+  def show
+  end
+
+  # GET /amendments/new
+  def new
+    @product = Amendment.new
+  end
+
+  # GET /amendments/1/edit
+  def edit
+  end
+
+  # POST /amendments
+  # POST /amendments.json
+  def create
+    @amendment = Amendment.new(amendment_params)
+
+    respond_to do |format|
+      if @amendment.save
+        format.html { redirect_to @amendment, notice: 'Amendment was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @amendment }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @amendment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /amendments/1
+  # PATCH/PUT /amendments/1.json
+  def update
+    respond_to do |format|
+      if @amendment.update(amendment_params)
+        format.html { redirect_to @amendment, notice: 'Amendment was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @amendment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /products/1
+  # DELETE /products/1.json
+  def destroy
+    @amendment.destroy
+    respond_to do |format|
+      format.html { redirect_to amendments_url }
+      format.json { head :no_content }
+    end
+  end
 
   private
+    def set_amendment
+      @amendment = Amendment.find(params[:id])
+    end
 
     def amendment_params
       params.require(:amendment).permit(:number, :type, :explanation, :user_id, :budget_id)
     end
-
 end
