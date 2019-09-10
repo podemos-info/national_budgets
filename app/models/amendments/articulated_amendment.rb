8aeaa31a958a89e967f8910a0c8a5df66c22d0a1
Jose <joseluispodemos@gmail.com>
2019-09-10 09:47:16 +0200

    Add amendment's type radio button

--- app/models/amendments/articulated_amendment.rb
+++ app/models/amendments/articulated_amendment.rb
@@ -1,4 +1,11 @@
 module Amendments
   class ArticulatedAmendment < Amendment
+    def self.model_name
+      Amendment.model_name
+    end
+
+    def type_name
+      'Al articulado'
+    end
   end
 end
--- app/models/amendments/standard_amendment.rb
+++ app/models/amendments/standard_amendment.rb
@@ -1,4 +1,11 @@
 module Amendments
   class StandardAmendment < Amendment
+    def self.model_name
+      Amendment.model_name
+    end
+
+    def type_name
+      'Alta/baja'
+    end
   end
 end
