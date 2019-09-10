f2990b62ea1e8601152b2112472947ad51182de6
Jose <joseluispodemos@gmail.com>
2019-09-10 12:45:33 +0200

    Restore eager_load to true and preload required classes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = true
+  config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -58,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  ActiveSupport::Reloader.to_prepare do
+    Dir['app/models/amendments/*.rb'].each {|file| require_dependency file}
+  end
 end
