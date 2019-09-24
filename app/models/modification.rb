d4e757731a1a06bdb90a10a81eaeddd31b0a7cd4
Jose <joseluispodemos@gmail.com>
2019-09-24 21:16:05 +0200

    Reorder modification class

--- app/models/modification.rb
+++ app/models/modification.rb
@@ -9,19 +9,19 @@ class Modification < ApplicationRecord
   belongs_to :article
   belongs_to :concept, optional: true
   belongs_to :subconcept, optional: true
-  validate :section_not_unique
   after_initialize :set_section
+  validate :section_not_unique
 
   def type_name
     self.class.to_s.demodulize.underscore.humanize
   end
 
-  def section_not_unique
-    errors.add(:section, 'is not unique in the amendment') if amendment.section && section != amendment.section
-  end
-
   def locked_section?
     @locked_section ||= amendment.modifications.where.not(id: id).count.positive?
+  end
+
+  def section_not_unique
+    errors.add(:section, 'is not unique in the amendment') if amendment.section && section != amendment.section
   end
 
   private
