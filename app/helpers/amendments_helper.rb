429f7e3d2303c00eb3ed3559456a464dab242294
Jose <joseluispodemos@gmail.com>
2019-10-04 14:01:29 +0200

    Solve commented issues in pull request

--- app/helpers/amendments_helper.rb
+++ app/helpers/amendments_helper.rb
@@ -2,7 +2,7 @@
 
 module AmendmentsHelper
   def collection_title(collection)
-    return t('helpers.views.empty_collection', model: collection.model_name.human(count: 0).downcase) unless collection.size.positive?
+    return t('helpers.views.empty_collection', model: collection.model_name.human(count: 0).downcase) if collection.empty?
 
     content_tag(:b, t('helpers.views.choose_model', model: collection.model_name.human.downcase)) + ':<br>'.html_safe
   end
@@ -19,7 +19,7 @@ module AmendmentsHelper
     link_to(fa_icon('times-circle'), path, class: 'text-danger', style: 'text-decoration: none')
   end
 
-  def amendment_section_link(amendment)
-    '<b>'.html_safe + Section.model_name.human + ':</b> '.html_safe + amendment.section&.full_title if amendment.section?
+  def amendment_section_label(amendment)
+    "<b>#{Section.model_name.human}:</b> #{amendment.section.full_title}".html_safe if amendment.section?
   end
 end
