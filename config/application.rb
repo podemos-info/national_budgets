a63d59edce550401c36a757122fac85bd8968447
Jose <joseluispodemos@gmail.com>
2019-10-10 14:52:47 +0200

    More descriptive variable name

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module NationalBudgets
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    preload_paths = %w[app/models/**/].freeze
-    config.eager_load_paths += Dir[*preload_paths]
+    eager_load_paths = %w[app/models/**/].freeze
+    config.eager_load_paths += Dir[*eager_load_paths]
   end
 end
