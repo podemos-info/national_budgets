78efe8eab0335d64464631470ae12db78fc0261d
Jose <joseluispodemos@gmail.com>
2019-10-21 09:53:25 +0200

    Custom Capybara's screenshot size

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,6 +7,10 @@ require 'capybara/rspec'
 Capybara.server = :puma, { Silent: true }
 Capybara.javascript_driver = :apparition
 
+Capybara.register_driver :apparition do |app|
+  Capybara::Apparition::Driver.new(app, {window_size: [1280, 720], inspector: true})
+end
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do |sample|
     if sample.metadata[:js]
