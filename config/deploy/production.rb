b4574e9241be3bc9ab9ce89ddcfa8c424fbf7960
leio10 <leiodd@gmail.com>
2020-01-10 11:32:54 +0100

    Add capistrano (#53)
    * Bump dependencies
    
    * Add Capistrano gems
    
    * Deploy settings.
    
    * Rubocop offenses fixes.

---
+++ config/deploy/production.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+server ENV['PRODUCTION_SERVER_HOST'], port: ENV['PRODUCTION_SERVER_PORT'], user: ENV['PRODUCTION_USER'], roles: %w[app db web]
+
+set :rails_env, :production
+set :user, ENV['PRODUCTION_USER']
+
+set :branch, :master
+
+after 'deploy:publishing', 'systemd:puma:restart'
