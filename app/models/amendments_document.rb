4e4dd23fd02441c938c13bd8d2a448ce997de7d7
Jose <joseluispodemos@gmail.com>
2020-01-27 16:29:28 +0100

    Add user to AmendmentsDocument model

--- app/models/amendments_document.rb
+++ app/models/amendments_document.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class AmendmentsDocument < ApplicationRecord
+  belongs_to :user, optional: false
   belongs_to :budget, optional: false
   belongs_to :section, optional: true
   has_one_attached :document
