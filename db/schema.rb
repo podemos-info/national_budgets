fe7cbc73ada3d5a4e2f2028172d9c247a7de98c1
Jose <joseluispodemos@gmail.com>
2020-01-27 16:18:20 +0100

    Migrated DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_110956) do
+ActiveRecord::Schema.define(version: 2020_01_27_140449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,18 @@ ActiveRecord::Schema.define(version: 2020_01_27_110956) do
     t.datetime "updated_at", null: false
     t.index ["budget_id"], name: "index_amendments_on_budget_id"
     t.index ["user_id"], name: "index_amendments_on_user_id"
+  end
+
+  create_table "amendments_documents", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "budget_id", null: false
+    t.bigint "section_id"
+    t.integer "file_type", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["budget_id"], name: "index_amendments_documents_on_budget_id"
+    t.index ["section_id"], name: "index_amendments_documents_on_section_id"
+    t.index ["user_id"], name: "index_amendments_documents_on_user_id"
   end
 
   create_table "articles", force: :cascade do |t|
